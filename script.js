43f4eb6067904e258202fc1c9d3b734518e7df21
h21tisto <h21tisto@du.se>
2022-11-29 22:57:44 +0100

    design

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var bookArray = {};
 let bookList = [];
+let bookArray = {};
 let filteredList = {};
 
 getAll().then((apiBooks) => (bookList = apiBooks));
-
+document.body.addEventListener('mouseover', (e) => renderBookDetails(e.target, filteredList));
 
 const searchField = document.getElementById('searchField');
 searchField.addEventListener('keyup', (e) => 
     renderBookList(
         bookList.filter(({title, author}) => {
             const searchTerm = e.target.value.toLowerCase();
-            if (searchTerm !='') return title.toLowerCase().indexOf(searchTerm) >= 0 || 
-                                        author.toLowerCase().indexOf(searchTerm) >= 0;
+            if (searchTerm !='') return title.toLowerCase().indexOf(searchTerm) >= 0 || author.toLowerCase().indexOf(searchTerm) >= 0;
         })
     )
 );
-
-document.body.addEventListener('mouseover', (e) => renderBookDetails(e.target, filteredList));
 
 async function renderBookDetails(element, filteredList)
 {
@@ -37,17 +34,9 @@ async function renderBookDetails(element, filteredList)
 
         document.body.addEventListener('mouseout', (e) => {
             var element = e.target;
-    
-            if(element.id === 'listItem')
-            {
-                var child = element.children[0];try {element.removeChild(child);} catch{};
-            }
+            if(element.id === 'listItem') var child = element.children[0]; try {element.removeChild(child);} catch{};
         });
-        
     };
-
-    
-    
 }
 
 function renderBookList(bookList) 
@@ -57,7 +46,5 @@ function renderBookList(bookList)
     const existingBookList = document.querySelector('.book-list');
     
     if (existingBookList) root.removeChild(existingBookList);
-
     if (bookList.length > 0) root.insertAdjacentHTML('beforeend', BookList(bookList));
-    
 }

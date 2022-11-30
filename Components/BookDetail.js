9c181ea783885d905896acfb8531174a6ee3f4d5
h21tisto <h21tisto@du.se>
2022-11-30 18:45:02 +0100

    design

--- Components/BookDetail.js
+++ Components/BookDetail.js
@@ -4,17 +4,23 @@ const BookDetail = (list) =>
 
     if (list['coverImage'] === '') // If the book has no cover image
     {
-        html = `<li id="bookDetail" class="mb-2 last:mb-0 px-8 mt-6 p-3 text-black last:border-b-0 border-t border-indigo-700 cursor-pointer">
-                    <p class="bookDetail__item mx-4 pt-4">Author: ${list['author']}</p>
-                    <p class="bookDetail__item mx-4">Title: ${list['title']}</p>
-                    <p class="bookDetail__item mx-4">Pages: ${list['pages']}</p>
-                    <p class="bookDetail__item mx-4">Release: ${list['releaseDate']}</p>
+        html = `<li id="bookDetail" class="mb-2 last:mb-0 px-8 pt-9 mt-6 p-3 text-black last:border-b-0 border-t border-black cursor-pointer">
+                    <div class="flex mx-auto space-x-24">
+                        <div class="inline-flex flex-col justify-center align-middle">
+                            <p class="bookDetail__item mx-4">Author: ${list['author']}</p>
+                            <p class="bookDetail__item mx-4">Title: ${list['title']}</p>
+                            <p class="bookDetail__item mx-4">Pages: ${list['pages']}</p>
+                            <p class="bookDetail__item mx-4">Release: ${list['releaseDate']}</p>
+                        </div>
+                        <img class="inline-flex object-scale-down h-48 w-auto" 
+                        src="https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg">
+                    </div>
                 </li>`;
     }
     else // If the book has a cover image
     {
         src = list['coverImage']
-        html = `<li id="bookDetail" class="mb-2 last:mb-0 px-8 pt-9 mt-6 p-3 text-black last:border-b-0 border-t border-indigo-700 cursor-pointer">
+        html = `<li id="bookDetail" class="mb-2 last:mb-0 px-8 pt-9 mt-6 p-3 text-black last:border-b-0 border-t border-black cursor-pointer">
                     <div class="flex mx-auto space-x-24">
                         <div class="inline-flex flex-col justify-center align-middle">
                             <p class="bookDetail__item mx-4">Author: ${list['author']}</p>
